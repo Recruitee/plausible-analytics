8b30ebae23100bc3c49c3300e6b7308cabdaa039
Jacek Kubiak <jacek.kubiak@recruitee.com>
2026-01-26 15:12:47 +0100

    Another backwards compatibility fix

--- priv/clickhouse_repo/migrations/20201020083739_add_event_metadata.exs
+++ priv/clickhouse_repo/migrations/20201020083739_add_event_metadata.exs
@@ -3,7 +3,8 @@ defmodule Plausible.ClickhouseRepo.Migrations.AddEventMetadata do
 
   def change do
     alter table(:events) do
-      add :meta, {:nested, {{:key, :string}, {:value, :string}}}
+      add :"meta.key", :"Array(String)"
+      add :"meta.value", :"Array(String)"
     end
   end
 end
