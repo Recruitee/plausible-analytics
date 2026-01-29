58eefd65460a237ddcf57f0d32f29bae2b626190
Jacek Kubiak <jacek.kubiak@recruitee.com>
2026-01-29 14:36:14 +0100

    Simplify configuration

--- config/config.exs
+++ config/config.exs
@@ -1,36 +1,42 @@
 import Config
 
 config :plausible,
-  ecto_repos: [Plausible.Repo, Plausible.ClickhouseRepo]
-
-config :plausible, PlausibleWeb.Endpoint, pubsub_server: Plausible.PubSub
-
-# Configures Elixir's Logger
-config :logger, :console,
-  format: "$time $metadata[$level] $message\n",
-  metadata: [:request_id]
-
-# Use Jason for JSON parsing in Phoenix
-config :phoenix, :json_library, Jason
-
-config :plausible,
-  # 30 minutes
-  session_timeout: 1000 * 60 * 30,
+  ecto_repos: [Plausible.Repo, Plausible.ClickhouseRepo],
   session_length_minutes: 30
 
-config :plausible, Plausible.ClickhouseRepo, loggers: [Ecto.LogEntry]
-
-config :plausible, Plausible.Repo,
-  timeout: 300_000,
-  connect_timeout: 300_000,
-  handshake_timeout: 300_000
+config :plausible, :ingestion,
+  buffer_size: 10_000,
+  flush_interval_ms: 5000
 
 config :plausible, :user_agent_cache,
   limit: 1000,
   stats: false
 
-config :plausible, :ingestion,
-  buffer_size: 10_000,
-  flush_interval_ms: 5000
+config :plausible, Oban,
+  repo: Plausible.Repo,
+  plugins: [{Oban.Plugins.Pruner, max_age: 2_592_000}],
+  queues: [
+    rotate_salts: 1
+  ],
+  crontab: [
+    {"0 0 * * *", Plausible.Tracking.Workers.RotateSalts}
+  ]
+
+config :plausible, Plausible.Telemetry.DatadogStatsd,
+  enabled: System.get_env("ENABLE_DD_STATS") || false
+
+config :plausible, PlausibleWeb.Endpoint, pubsub_server: Plausible.PubSub
+
+config :logger, :console,
+  level: :info,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+config :opentelemetry_exporter,
+  endpoints: [],
+  otlp_protocol: :http_protobuf,
+  log_level: :error
+
+config :phoenix, :json_library, Jason
 
 import_config "#{config_env()}.exs"
